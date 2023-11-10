a902fa192cc3af95f7fb9e1a67150266a03d1928
LeaHerrera <lea.herrera.7e7@itb.cat>
2023-11-10 15:02:54 +0100

    guardando

--- index.js
+++ index.js
@@ -1,26 +1,58 @@
-let palabras = ["amigo","gafas","focas"];
+let palabras = ["AMIGO","GAFAS","FOCAS"];
 let numero = (Math.random()*100) %3;
-var palabraSecreta = palabras[Math.ceil(numero)-1];
+var palabraSecreta = palabras[Math.ceil(numero)-1].split("");
 console.log("Palabra Secreta :" , palabraSecreta);
+
+var contador = 0;
 
 function prueba() {
     let imputNumero = document.getElementById("inputFoormulario")
-    var palabraPrueba = imputNumero.value ;
+    var newDiv = document.getElementById("resultados");
+    var palabraPrueba = imputNumero.value.toUpperCase() ;
+
     imputNumero.value = "" ;
     console.log("Palabra Prueba :" , palabraPrueba) ;
 
-    if (palabraPrueba.length == 5){
-        var newDiv = document.createElement("div")
-        newDiv.id = "resultados"
+    if(contador < 5){ 
+        if (palabraPrueba.length == 5){
+            
+            var palabraSeparada = palabraPrueba.split("");
+            console.log("palabraSeparada :",palabraSeparada);
 
-        let celdas = document.createElement("div")
-        caracter.classList.add("celdas")
-        newDiv.appendChild(celdas)
+            for(i = 0 ; i < 5; i++){
+                let celdas = document.createElement("div");
+                celdas.classList.add("celdas");
+                
+                let caracter = document.createElement("p");
+                caracter.classList.add("caracter");
+                
+                newDiv.appendChild(celdas);
+                celdas.appendChild(caracter);
 
-        let caracter = document.createElement("p")
-        newDiv.appendChild(caracter)
+                let text = palabraSeparada[i];
+                caracter.innerHTML = text;
 
-    }else{
-        alert("La palabra debe tener 5 caracteres", "intentalo nuevamente!")
+                if( palabraSeparada[i] == palabraSecreta[i] ) {
+                    celdas.classList.add("verde");
+                } else if ( palabraSecreta.includes(palabraSeparada[i]) ){
+                    celdas.classList.add("amarillo");
+                } else {
+                    celdas.classList.add("negro");
+                }
+            }
+            contador++;
+            
+            let mensaje = document.getElementById("textMensaje");
+            let text = `Intento numero ${contador}, te quedan ${5-contador} intentos`;
+            mensaje.innerHTML = text;
+
+        }else{
+            alert("La palabra debe tener 5 caracteres", "intentalo nuevamente!");
+        }
+
+    } else {
+        let mensaje = document.getElementById("textMensaje");
+        let text = `Intentos maximos :( , recarga e intentalo nuevamente!`;
+        mensaje.innerHTML = text;
     }
 } 
