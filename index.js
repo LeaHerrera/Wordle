a733fea62bde4ab9fd49a381970ea524191538b4
LeaHerrera <lea.herrera.7e7@itb.cat>
2023-11-10 15:34:59 +0100

    guardando

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let palabras = ["AMIGO","GAFAS","FOCAS"];
-let numero = (Math.random()*100) %3;
+let palabras = ["AMIGO","GAFAS","FOCAS","AGUA", "AMOR", "BELLO", "CAMPO", "CARNE", "CLAVE", "COLOR", "CRUZ", "DONAR", "ELEVO", "FLOR", "GENTE", "HUMOR", "JUEGO", "NIEVE", "OBRAS", "PLATA", "QUEMA", "RAMA", "SALUD", "TECHO", "VISTA", "ALMA", "CALOR", "DESEO", "FUEGO", "GRANO", "HOJAS", "IGUAL", "MAGIA", "PESAR", "QUEDA", "RIQUE", "UNION", "YERBA", "ZUMBA", "ABETO", "BURRO", "CIELO", "DANZA", "FELIZ", "HUEVO", "JAMON", "LIMON", "MANGO", "NADAR", "RETRO", "SALTO", "TIGRE", "VALOR", "BRISA", "CIEGA", "FLAMA", "GASTO", "HEROE", "JEFES", "LOMOS", "MACRO", "NENES", "PESOS", "QUEDA", "RELOJ", "SURCO", "VENDE"];
+let numero = (Math.random()*10000) % 70;
 var palabraSecreta = palabras[Math.ceil(numero)-1].split("");
 console.log("Palabra Secreta :" , palabraSecreta);
 
@@ -19,11 +19,13 @@ function prueba() {
             var palabraSeparada = palabraPrueba.split("");
             console.log("palabraSeparada :",palabraSeparada);
 
+            var letrasBien = 0;
+
             for(i = 0 ; i < 5; i++){
-                let celdas = document.createElement("div");
+                var celdas = document.createElement("div");
                 celdas.classList.add("celdas");
                 
-                let caracter = document.createElement("p");
+                var caracter = document.createElement("p");
                 caracter.classList.add("caracter");
                 
                 newDiv.appendChild(celdas);
@@ -34,6 +36,7 @@ function prueba() {
 
                 if( palabraSeparada[i] == palabraSecreta[i] ) {
                     celdas.classList.add("verde");
+                    letrasBien++
                 } else if ( palabraSecreta.includes(palabraSeparada[i]) ){
                     celdas.classList.add("amarillo");
                 } else {
@@ -45,6 +48,27 @@ function prueba() {
             let mensaje = document.getElementById("textMensaje");
             let text = `Intento numero ${contador}, te quedan ${5-contador} intentos`;
             mensaje.innerHTML = text;
+
+            if ( letrasBien == 5){
+                let mensaje = document.getElementById("textMensaje");
+                let text = `Felicidades! :D , palabra correcta!`;
+                mensaje.innerHTML = text;
+
+                let uno = document.getElementById("uno");
+                let dos = document.getElementById("dos");
+                let tres = document.getElementById("tres");
+                let cuatro = document.getElementById("cuatro");
+                let cinco = document.getElementById("cinco");
+
+                uno.innerHTML = palabraSecreta[0];
+                dos.innerHTML = palabraSecreta[1];
+                tres.innerHTML = palabraSecreta[2];
+                cuatro.innerHTML = palabraSecreta[3];
+                cinco.innerHTML = palabraSecreta[4];
+
+                alert("Has adivinado la palabra!, Felicidades! :D");
+                alert("Si quieres volcer a jugar, recarga la pagina");
+            }
 
         }else{
             alert("La palabra debe tener 5 caracteres", "intentalo nuevamente!");
